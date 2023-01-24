6c1d1dfdcd5c194e16eaa5180df23af5c6656ad7
Cody Hilborn <codyhilborn11@gmail.com>
2023-01-24 03:35:16 +0000

    Cleaned up cide and added additional comments

--- express_server.js
+++ express_server.js
@@ -13,35 +13,44 @@ const urlDatabase = {
   '9sm5xK': 'http://www.google.com',
 };
 
+const generateRandomString = function() {
+  //--> Generates a random string of 6 alpha-numeric characters.
+  return Math.random().toString(36).slice(2, 8);
+};
+
 app.get('/', (req, res) => {
   res.send('Hello!');
 });
 
 app.get('/urls', (req, res) => {
+  // --> Get route for MyURLs tab, showing table of previoiusly created TinyURLS and corresponding long URLs 
   const templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
 });
 
 app.post('/urls', (req, res) => {
-  console.log(req.body);
-  res.send('Ok');
+  // console.log(req.body);
+  // --> Creating new random string, assigning it as key in urlDatabase w/ form input as value.
+  const newShortURL = generateRandomString();
+  urlDatabase[newShortURL] = req.body.longURL;
+  console.log(urlDatabase);
+  res.send();
 });
 
 app.get('/urls/new', (req, res) => {
+  //  --> Get route for the Create TinyURL page w/ form.
   res.render('urls_new');
 });
 
 app.get('/urls/:id', (req, res) => {
+  // --> Accessed by adding the short URL in place of ':id' in the browser.
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render('urls_show', templateVars);
 });
 
 
-const generateRandomString = function() {
-  return Math.random().toString(36).slice(2, 8);
-};
 
-
+// --> Server listener
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
